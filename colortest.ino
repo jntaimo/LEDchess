3fa7f23fb988330dc4d939cc062c319daa8bb345
jntaimo <jntaimo@gmail.com>
2021-01-11 13:54:34 +0100

    Displayed all of the possible moves from the current board configuration.

--- colortest.ino
+++ colortest.ino
@@ -44,7 +44,15 @@ char board[129] = {
     9,  9,  9,  9,  9,  9,  9,  9,  0,  0,  0,  0,  0,  0,  0,  0,
     14, 12, 13, 15, 11, 13, 12, 14, 0,  0,  0,  0,  0,  0,  0,  0,
 };
-
+const uint8_t move_offsets[] = {
+    15,  16,  17,   0,                              // black pawns
+    15, -16, -17,   0,                              // white pawns
+     1,  16,  -1, -16,   0,                         // rooks
+     1,  16,  -1, -16,  15, -15,  17, -17,   0,     // queens, kings and bishops
+    14, -14,  18, -18,  31, -31,  33, -33,   0,     // knights
+     3,  -1,  12,  21,  16,  7, 12                  // starting indices for each piece type in order
+                                                    //   white pawns, black pawns, kings, knights, bishops, rooks, queens
+};     
 void print_board(){
     uint8_t i = 0;
     char piece = 0;
@@ -80,6 +88,73 @@ void display_board(){
     strip.Show();
 }
 
+void search(char side){
+    uint8_t index = 0;
+    //loop over board squares
+    while(index<128){
+        if((index & 0x88) == 0){
+            char piece = board[index];
+            //if the piece is on the right side
+            if(piece & side){
+                char piece_type = piece & 7;
+                //moves to the starting indices in offsets
+                uint8_t directions = move_offsets[piece_type + 30];
+                ++directions;
+
+                //loop over more offsets
+                while(move_offsets[directions]){
+                    uint8_t step_vector = move_offsets[directions];
+                    ++directions;
+                    
+                    uint8_t source_square = index;
+                    uint8_t target_square = source_square;
+                    uint8_t captured_piece = 0;
+
+                    //loop over slider ray
+                    while(captured_piece == 0){
+                        target_square += step_vector;
+                        uint8_t captured_square = target_square;
+
+                        if(target_square & 0x88) break;//off board
+
+                        captured_piece = board[captured_square];
+
+                        if(captured_piece & side) break; //same side
+
+                        if(piece_type < 3 && (!(step_vector & 7)) != !captured_piece) break;//pawn limitations
+
+                        //make move
+                        board[captured_square] = board[source_square] = 0;
+                        board[target_square] = piece;
+
+                        //pawn promotion
+                        if(piece_type < 3){
+                            if(target_square + step_vector + 1 & 0x80){
+                                board[target_square] |= 7;
+                            }
+                        }
+                        print_board();
+                        display_board();
+                        delay(500);
+                        //take back move
+                        board[target_square] = 0;
+                        board[source_square] = piece;
+                        board[captured_square] = captured_piece;
+
+                        //fake capture for leapers
+                        captured_piece += (piece_type < 5);
+
+                        //unfake capture for double pawn move
+                        if(piece_type < 3 && (6 * side + (target_square & 0x70) == 0x80)){
+                           --captured_piece; 
+                        }             
+                    }
+                }
+            }
+        }
+        ++index;
+    }
+}
 void setup(){
     Serial.begin(115200);
     while (!Serial);
@@ -88,7 +163,7 @@ void setup(){
     Serial.println("Initializing...");
     Serial.flush();
     for(uint8_t i=0; i<14; ++i){
-        //piece_colors[i].Darken(100);
+        piece_colors[i].Darken(80);
         //piece_colors[i]  = colorGamma.Correct(piece_colors[i]);
     }
     strip.Begin();
@@ -96,5 +171,9 @@ void setup(){
     //print_board();
 }
 
-void loop(){}
+void loop(){
+    delay(1000);
+    search(white);
+    search(black);    
+}
 
