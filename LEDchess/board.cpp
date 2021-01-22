2f2204c6d5842824298a13213df6c642d25c8a79
jntaimo <jntaimo@gmail.com>
2021-01-22 12:06:02 +0100

    Started basic framework for chessboard

--- LEDchess/board.cpp
+++ LEDchess/board.cpp
@@ -1,14 +1,6 @@
 //Author: Joseph Ntaimo
 //File
 #include "board.h"
-class Bitboard
-{
-private:
-    /* data */
-public:
-    Bitboard(/* args */);
-    ~Bitboard();
-};
 
 Bitboard::Bitboard(/* args */)
 {
--- LEDchess/board.h
+++ LEDchess/board.h
@@ -1,7 +1,41 @@
 
 #ifndef HEADER_BOARD
-    #define HEADER_BOARD
+#define HEADER_BOARD
 
+#include <cstdint>
+class Bitboard
+{
+private:
+    uint16_t _nummoves = 0; 
+    
+    bool _blackcastled = false;
+    bool _white_castled = false;
+
+    char _board[129];
+    char _pieces[17] = ".-pknbrq-P-KNBRQ";
+    char * _pieces_ascii = nullptr
+
+    void _init_board();
+    void _clear_board();
+    void _reset_board();
+    void _init();
+
+public:
+    Bitboard(/* args */);
+    ~Bitboard();
+
+    //move making
+    bool valid_move();
+    bool make_move();
+    bool undo_move(uint16_t nummoves = 1);
+    void print_board();
+
+    //getters
+    char * get_board();
+    char * get_moves();
+    uint16_t * get_nummoves();
+
+};
 
 
 #endif
