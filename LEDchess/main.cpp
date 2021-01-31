dfa5701742452185f817a38930f20378a1fe63c4
jntaimo <jntaimo@gmail.com>
2021-01-30 21:37:30 -0600

    Tried printing the copy of the initial position - printing works, board representation doesn't

--- LEDchess/main.cpp
+++ LEDchess/main.cpp
@@ -3,18 +3,7 @@
 
 void printboard(JN::Bitboard bitboard){
     const char * b = bitboard.get_board();
-    // for(uint8_t i=0;i<121;i++)printf(" %c",i&8&&(i+=7)?10:JN::pieces[b[i]&15]);
-    uint8_t i = 0;
-    while(i < 128){
-        if (!(i & 0x88)){
-            printf("%c ",JN::pieces[b[i] & 15]);
-            ++i;
-        } else {
-            printf("\n");
-            i += 8;
-        }
-    }
-
+    for(uint8_t i=0;i<121;i++)printf(" %c",i&8&&(i+=7)?10:JN::pieces[b[i]&15]);
     printf("\n");
 }
 
@@ -22,5 +11,9 @@ int main(){
     printf("Starting up...\n");
     JN::Bitboard bitboard = JN::Bitboard();
     for (uint8_t i = 5; i > 0; --i) printboard(bitboard);
+    printf("Normal empty board\n\n\n");
+    for (uint8_t i = 5; i > 0; --i){
+        for(uint8_t i=0;i<121;i++)printf(" %c",i&8&&(i+=7)?10:JN::pieces[JN::newboard[i]&15]);
+    }
     return 0;
 }
