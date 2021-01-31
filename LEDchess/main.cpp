4fcb72def50c64a59bc2b8ac57d7e809f53250ef
jntaimo <jntaimo@gmail.com>
2021-01-30 18:15:35 -0600

    Printed the board 5 times - changes values

--- LEDchess/main.cpp
+++ LEDchess/main.cpp
@@ -4,11 +4,12 @@
 void printboard(JN::Bitboard bitboard){
     const char * b = bitboard.get_board();
     for(uint8_t i=0;i<121;i++)printf(" %c",i&8&&(i+=7)?10:".?+nkbrq?*?NKBRQ"[b[i]&15]);
+    printf("\n");
 }
 
 int main(){
     printf("Starting up...\n");
     JN::Bitboard bitboard = JN::Bitboard();
-    printboard(bitboard);
+    for (uint8_t i = 5; i > 0; --i) printboard(bitboard);
     return 0;
 }
